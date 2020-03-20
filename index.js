c68c9feea36d8220ca1deebc57f135523aa977f3
ancao2504 <pullnolovesmile@gmail.com>
2020-03-20 12:56:24 +0700

    push code cronjob

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 let request = require('request');
 let axios = require('axios');
-
-request('https://coronaapiwom.herokuapp.com/apidata', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        let today = new Date();
-        let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        let json = JSON.parse(body)
-        let messenger = '';
-        let content = 'THỐNG KÊ TÌNH HÌNH DỊCH BỆNH nCoV-19. CẬP NHẬT NGÀY ' + date + ' LÚC ' + time;
-        sendMessageTelegram(content.bold())
-        for (let index = 0; index < json.length; index++) {
-            let countryName = json[index].Country_Name;
-            let totalCases = json[index].Total_Cases;
-            let totalDeaths = json[index].Total_Deaths;
-            let totalRecovered = json[index].Total_Recovered;
-            let seriousCases = json[index].Serious_Cases;
-            let arrayCountry = ['China', 'Vietnam', 'Italy', 'USA', 'UK', 'Malaysia', 'S. Korea', 'Singapore', 'Indonesia', 'Thailand', 'Philippines', 'India', 'Japan', 'Iran'];
-            arrayCountry.forEach(function(valueCountry){
-                if(countryName == valueCountry) {
-                    messenger = countryName +' hiên tại đã có '+ totalCases +' ca nhiễm, '+ seriousCases +' ca nghiêm trọng, '+ totalDeaths +' ca tử vong và '+ totalRecovered +' đã phục hồi';
-                    sendMessageTelegram(messenger.italics())
-                }
-            }) 
-        }
-    }else {
-        sendMessageTelegram('API NO CONNECT: ' + error)
-    }
+let express = require('express');
+let app = express(); 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
+const server = app.listen(7000, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
+function todayReport() {
+    request('https://coronaapiwom.herokuapp.com/apidata', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let today = new Date();
+            let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+            let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            let json = JSON.parse(body)
+            let messenger = '';
+            let content = 'THỐNG KÊ TÌNH HÌNH DỊCH BỆNH nCoV-19. CẬP NHẬT NGÀY ' + date + ' LÚC ' + time;
+            sendMessageTelegram(content.bold())
+            for (let index = 0; index < json.length; index++) {
+                let countryName = json[index].Country_Name;
+                let totalCases = json[index].Total_Cases;
+                let totalDeaths = json[index].Total_Deaths;
+                let totalRecovered = json[index].Total_Recovered;
+                let seriousCases = json[index].Serious_Cases;
+                let arrayCountry = ['China', 'Vietnam', 'Italy', 'USA', 'UK', 'Malaysia', 'S. Korea', 'Singapore', 'Indonesia', 'Thailand', 'Philippines', 'India', 'Japan', 'Iran'];
+                arrayCountry.forEach(function(valueCountry){
+                    if(countryName == valueCountry) {
+                        messenger = countryName +' hiên tại đã có '+ totalCases +' ca nhiễm, '+ seriousCases +' ca nghiêm trọng, '+ totalDeaths +' ca tử vong và '+ totalRecovered +' đã phục hồi';
+                        sendMessageTelegram(messenger.italics())
+                    }
+                }) 
+            }
+        }else {
+            sendMessageTelegram('API NO CONNECT: ' + error)
+        }
+    });
+}
+
 
 function sendMessageTelegram(textMessage) {
     let payload = {
@@ -49,17 +59,16 @@ function sendMessageTelegram(textMessage) {
     });
 }
 
-let CronJob = require('cron').CronJob;
-let job = new CronJob('00 00 6 * * 1-7', function() {
-  /*
-   * Runs every weekday (Monday through Friday)
-   * at 11:30:00 AM. It does not run on Saturday
-   * or Sunday.
-   */
-  }, function () {
-    /* This function is executed when the job stops */
+const cron = require('cron');
+const job = new cron.CronJob({
+  cronTime: '00 00 13 * * 0-6', // Chạy Jobs vào 23h30 hằng đêm
+  onTick: function() {
+    todayReport();
+    console.log('Cron jub runing...');
   },
-  true, /* Start the job right now */
-  'Asia/Ho_Chi_Minh' /* Time zone of this job. */
-);
+  start: true, 
+  timeZone: 'Asia/Ho_Chi_Minh' // Lưu ý set lại time zone cho đúng 
+});
+
 job.start();
+
