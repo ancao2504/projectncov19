9d1cf128b8edac4493600f5c5a8680d565279c96
ancao2504 <pullnolovesmile@gmail.com>
2020-04-01 21:42:59 +0700

    edit apincov19

--- index.js
+++ index.js
@@ -2,39 +2,46 @@ let request = require('request');
 let axios = require('axios');
 let express = require('express');
 let app = express(); 
+let URL = 'https://viruscoronaapi.herokuapp.com/worldometers';
 const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => res.send('THỐNG KÊ TÌNH HÌNH DICH BỆNH ĐƯỢC VIẾT BẰNG NODEJS'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-request('https://coronaapiwom.herokuapp.com/apidata', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        let today = new Date();
-        let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        let json = JSON.parse(body)
-        let messenger = '';
-        let content = 'THỐNG KÊ TÌNH HÌNH DỊCH BỆNH nCoV-19. CẬP NHẬT NGÀY ' + date + ' LÚC ' + time;
-        sendMessageTelegram(content.bold())
-        for (let index = 0; index < json.length; index++) {
-            let countryName = json[index].Country_Name;
-            let totalCases = json[index].Total_Cases;
-            let newCases = json[index].New_Cases;
-            let totalDeaths = json[index].Total_Deaths;
-            let newDeaths = json[index].New_Deaths;
-            let totalRecovered = json[index].Total_Recovered;
-            let seriousCases = json[index].Serious_Cases;
-            let arrayCountry = ['China', 'Vietnam', 'Italy', 'USA', 'UK', 'Malaysia', 'S. Korea', 'Singapore', 'Indonesia', 'Thailand', 'Philippines', 'India', 'Japan', 'Iran'];
-            arrayCountry.forEach(function(valueCountry){
-                if(countryName == valueCountry) {
-                    messenger = countryName +' hiên tại đã có '+ totalCases +' ca nhiễm, trong đó: '+ newCases +' ca mới, '+ seriousCases +' ca nghiêm trọng, '+ totalDeaths +' ca tử vong, '+ newDeaths + ' ca tử vong mới ' + 'và '+ totalRecovered +' đã phục hồi';
-                    sendMessageTelegram(messenger.italics())
-                }
-            }) 
-        }
-    }else {
-        sendMessageTelegram('API NO CONNECT: ' + error)
-    }
-});
+extractDataNcov19(URL)
+function extractDataNcov19(url) {
+	request(url, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	        let today = new Date();
+	        let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+	        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	        let json = JSON.parse(body)
+	        let messenger = '';
+	        let content = 'THỐNG KÊ TÌNH HÌNH DỊCH BỆNH nCoV-19. CẬP NHẬT NGÀY ' + date + ' LÚC ' + time;
+	        sendMessageTelegram(content.bold())
+	        for (let index = 0; index < json.length; index++) {
+	            let countryName = json[index].Country_Name;
+	            let totalCases = json[index].Total_Cases;
+	            let newCases = json[index].New_Cases;
+	            let totalDeaths = json[index].Total_Deaths;
+	            let newDeaths = json[index].New_Deaths;
+	            let totalRecovered = json[index].Total_Recovered;
+	            let seriousCases = json[index].Serious_Cases;
+	            let arrayCountry = ['China', 'Vietnam', 'Italy', 'USA', 'UK', 'Malaysia', 'S. Korea', 'Singapore', 'Indonesia', 'Thailand', 'Philippines', 'India', 'Japan', 'Iran'];
+	            arrayCountry.forEach(function(valueCountry){
+	                if(countryName == valueCountry) {
+	                    messenger = countryName +' hiên tại đã có '+ totalCases +' ca nhiễm, trong đó: '+ newCases +' ca mới, '+ seriousCases +' ca nghiêm trọng, '+ totalDeaths +' ca tử vong, '+ newDeaths + ' ca tử vong mới ' + 'và '+ totalRecovered +' đã phục hồi';
+	                    sendMessageTelegram(messenger.italics())
+	                }
+	            }) 
+	        }
+	    }else {
+	        sendMessageTelegram('API NO CONNECT: ' + url)
+	    }
+	});
+}
+
 
 function sendMessageTelegram(textMessage) {
     let payload = {
@@ -56,15 +63,4 @@ function sendMessageTelegram(textMessage) {
     });
 }
 
-// const cron = require('cron');
-// const job = new cron.CronJob({
-//   cronTime: '00 00 13 * * 0-6', // Chạy Jobs vào 23h30 hằng đêm
-//   onTick: function() {
-//     console.log('Cron jub runing...');
-//   },
-//   start: true, 
-//   timeZone: 'Asia/Ho_Chi_Minh' // Lưu ý set lại time zone cho đúng 
-// });
-
-// job.start();
 
